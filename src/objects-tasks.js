e6f7b6ea83951e7d1696b1cedee7228a27a0f02e
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-02-17 16:37:29 +0400

    complete sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -177,8 +177,19 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticketCost = 25;
+  let change = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] > ticketCost) {
+      if (change < queue[i] - ticketCost) {
+        return false;
+      }
+    } else {
+      change += queue[i];
+    }
+  }
+  return true;
 }
 
 /**
